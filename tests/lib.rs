7a268a678a0115f48d9e848735ed21372fa45ebf
Henning Meyer <tutmann@gmail.com>
2025-06-17 19:56:06 +0000

    make ready to publish

---
+++ tests/lib.rs
@@ -0,0 +1,12 @@
+//! Integration tests for the brainstem library
+
+#[test]
+fn compile_and_run_brainstem() {
+    let src = r#"var x = 2 + 2; putc("0" + x);"#;
+    let bf = brainstem::compile_brain_stem(src).expect("compile");
+    let mut output = Vec::new();
+    let mut input = std::io::empty();
+    let steps = brainstem::run_program::<u8>(&bf, &mut input, &mut output, Some(1000)).expect("run");
+    assert!(steps > 0);
+    assert_eq!(output, b"4");
+}
